4a64ba3852cbb0ce8ff3d5b6a94c36e8dd287e33
Haim@DESKTOP-NOFSN64 <>
2021-01-28 00:11:49 -0600

    Do styles to each component

--- src/app/warning/warning.component.ts
+++ src/app/warning/warning.component.ts
@@ -1,7 +1,16 @@
 import { Component } from "@angular/core";
 @Component({
     selector: 'app-warning',
-    templateUrl: './warning.component.html'
+    templateUrl: './warning.component.html',
+    styles: [
+        `
+        p {
+            padding: 10px;
+            background-color: mistyrose;
+            border: 1px solid red;   
+        }
+        `
+    ]
 })
 export class WarningComponent {
 
