f3df3efa721f335c327facbc88338f430b09129e
Haim <>
2021-01-30 22:49:36 -0600

    add servers component, title: Two-way-databinding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SuccessComponent } from './success/success.component';
 import { WarningComponent } from './warning/warning.component';
-
+import { ServersComponent } from './servers/servers.component';
 @NgModule({
   declarations: [
     AppComponent,
     SuccessComponent,
-    WarningComponent
+    WarningComponent,
+    ServersComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
--- src/app/warning/warning.component.ts
+++ src/app/warning/warning.component.ts
@@ -1,16 +1,7 @@
 import { Component } from "@angular/core";
 @Component({
     selector: 'app-warning',
-    templateUrl: './warning.component.html',
-    styles: [
-        `
-        p {
-            padding: 10px;
-            background-color: mistyrose;
-            border: 1px solid red;   
-        }
-        `
-    ]
+    templateUrl: './warning.component.html'
 })
 export class WarningComponent {
     name: string = 'Haim Guel Quiroz';
